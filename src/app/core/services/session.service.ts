09c3a4879dd989313dc5f58ab7ef6cb0108700cf
Muhammad Irfan Shiddiq <irfams99@gmail.com>
2024-07-31 21:46:13 +0700

    wip: :rocket: extract jwtClaims on sessionService

--- src/app/core/services/session.service.ts
+++ src/app/core/services/session.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from "@angular/core";
+import { jwtDecode } from "jwt-decode";
+import { JwtClaims } from "../../shared/models/jwt-claims.model";
+import { UserInfo } from "../../shared/models/user-info.model";
+
 
 @Injectable({
   providedIn: "root",
@@ -13,15 +17,36 @@ export class SessionService {
     return this.storage.getItem(key);
   }
 
+  clearSession(): void {
+    this.storage.clear()
+  }
+
   isAuthenticated(): boolean {
-    return this.isTokenValid(this.get("token"));
+    return (
+      this.isTokenValid(this.get('token')) &&
+      this.isTokenExpired(this.get('token'))
+    )
   }
 
-  isTokenValid(token: string | null): boolean {
-    return token ? token.split(".").length === 3 : false;
+  private isTokenValid(token: string | null): boolean {
+    return token ? token.split('.').length === 3 : false
   }
 
-  // getCllaim(): string | null {
-    
-  // }
+  private isTokenExpired(token: string | null): boolean {
+    try {
+      const { exp } = this.getJwtClaims(token) || { exp: null }
+      return !!exp && Date.now() < exp * 1000
+    } catch {
+      return false
+    }
+  }
+
+  private getJwtClaims(token: string | null): JwtClaims | null {
+    return token ? jwtDecode<JwtClaims>(token) : null
+  }
+
+  public getUserInfo(): UserInfo | null {
+    const { id, email, username, roles } = this.getJwtClaims(this.get('token')) || {}
+    return id && email && username && roles ? { id, email, username, roles } : null
+  }
 }
--- src/app/shared/models/jwt-claims.model.ts
+++ src/app/shared/models/jwt-claims.model.ts
@@ -1,7 +1,14 @@
-export interface JwtClaims {
-    sub: string
-    iss: string
-    name: string
-    email: string
-    role: string
+interface JwtStandardClaims {
+  iss?: string; // Issuer
+  sub?: string; // Subject
+  exp: number; // Expiration time (as a Unix timestamp)
+  iat?: number; // Issued at (as a Unix timestamp)
+  // Add any other standard JWT claims you might need
+}
+
+export interface JwtClaims extends JwtStandardClaims {
+  id: string;
+  email: string;
+  username: string;
+  roles: string;
 }
