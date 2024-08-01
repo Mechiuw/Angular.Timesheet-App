fe09fcd238600df94f6a105ecca96a946a2e68dd
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-08-01 16:23:04 +0700

    [FEATURE] Added Login ui and Verification page ui

---
+++ src/app/login-ui/login-ui.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-ui',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-ui.component.html',
+  styleUrl: './login-ui.component.scss'
+})
+export class LoginUiComponent {
+
+}
