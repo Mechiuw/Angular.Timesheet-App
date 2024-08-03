c926a62f052b059039e54e678a47666fd6be4139
bharaa <bharalfhani@gmail.com>
2024-08-03 12:35:30 +0700

    style: :art: adjust view for profile and add loading animation when save profile

--- src/app/modules/layout/components/navbar/profile-menu/your-profile/your-profile.component.ts
+++ src/app/modules/layout/components/navbar/profile-menu/your-profile/your-profile.component.ts
@@ -1,20 +1,44 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-your-profile',
   standalone: true,
-  imports: [AngularSvgIconModule, CommonModule, FormsModule],
+  imports: [
+    AngularSvgIconModule,
+    CommonModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+  ],
   templateUrl: './your-profile.component.html',
-  styleUrl: './your-profile.component.scss'
+  styleUrl: './your-profile.component.scss',
 })
 export class YourProfileComponent {
   name: string = 'Johnny Storm';
   username: string = 'johnny123';
   email: string = 'johnny@email.com';
   editMode: boolean = false;
+  loading: boolean = false;
+
+  constructor(private location: Location) {}
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  load() {
+    this.loading = true;
+
+    setTimeout(() => {
+      this.loading = false;
+      this.editMode = false;
+    }, 2000);
+  }
 
   toggleEditMode(): void {
     this.editMode = !this.editMode;
@@ -24,6 +48,6 @@ export class YourProfileComponent {
     this.name = newName;
     this.username = newUsername;
     this.email = newEmail;
-    this.editMode = false;
+    this.load();
   }
 }
