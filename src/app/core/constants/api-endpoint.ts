841c993640cb777ac03aa0bff6d44020399106fc
Galih Prastya Juansaputra <galih.juansaputra@gmail.com>
2024-08-05 16:20:28 -0400

    wip: table list in user master data with global field search

--- src/app/core/constants/api-endpoint.ts
+++ src/app/core/constants/api-endpoint.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://10.10.102.46:8080/api/v1';
+export const API_BASE_URL = 'https://sure-pika-easy.ngrok-free.app/api/v1';
 export const API_ENDPOINT = {
     AUTH: {
         LOGIN: `${API_BASE_URL}/login`,
--- src/app/modules/user/components/user-list/user-list.component.ts
+++ src/app/modules/user/components/user-list/user-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { TableModule } from 'primeng/table';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Table, TableModule } from 'primeng/table';
 import { User } from '../../models/user.model';
 import { TagModule } from 'primeng/tag';
 import { IconFieldModule } from 'primeng/iconfield';
@@ -7,38 +7,69 @@ import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { DropdownModule } from 'primeng/dropdown';
+import { NftHeaderComponent } from '../../../dashboard/components/nft/nft-header/nft-header.component';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [TableModule, TagModule, IconFieldModule, InputTextModule, InputIconModule, MultiSelectModule, DropdownModule],
+  imports: [
+    InputTextModule,
+    TagModule,
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    TableModule,
+    TagModule,
+    IconFieldModule,
+    InputTextModule,
+    InputIconModule,
+    MultiSelectModule,
+    DropdownModule,
+    NftHeaderComponent,
+  ],
 
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.scss'
+  styleUrl: './user-list.component.scss',
 })
 export class UserListComponent implements OnInit {
+
+  @ViewChild('dt1') dt: Table | undefined;
+
+  searchValue: string | undefined;
+
+  clear(table: Table) {
+    table.clear();
+    this.searchValue = '';
+  }
+
+  applyFilterGlobal($event: any, stringVal: any) {
+    this.dt!.filterGlobal(($event.target as HTMLInputElement).value, stringVal);
+  }
+
   users: User[] = [];
   ngOnInit(): void {
     this.users = [
       {
-        "email": "ranchodas@gmail.com",
-        "name": "rancho",
-        "role": "manager",
-        "status": "active"
+        email: 'ranchodas@gmail.com',
+        name: 'rancho',
+        role: 'manager',
+        status: 'active',
       },
       {
-        "email": "jarjit@gmail.com",
-        "name": "Jarjit",
-        "role": "benefit",
-        "status": "active"
+        email: 'jarjit@gmail.com',
+        name: 'Jarjit',
+        role: 'benefit',
+        status: 'active',
       },
       {
-        "email": "mael@gmail.com",
-        "name": "mael",
-        "role": "trainer",
-        "status": "active"
-      }
-    ]
+        email: 'mael@gmail.com',
+        name: 'mael',
+        role: 'trainer',
+        status: 'active',
+      },
+    ];
   }
-
 }
