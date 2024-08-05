6fe5baf498170cafc2c32dad7a852da96e19611d
Fakhrul <barz.develop@gmail.com>
2024-08-06 01:28:42 +0700

    fix: :construction: spelling

--- src/app/modules/timesheet/components/test/test.component.ts
+++ src/app/modules/timesheet/components/test/test.component.ts
@@ -4,11 +4,13 @@ import { WorkOption } from '../../model/timesheet';
 import { TimesheetService } from '../../services/timesheet.service';
 import { TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [TableModule, CommonModule],
+  imports: [TableModule, CommonModule, MatFormFieldModule, MatSelectModule],
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.scss'],
 })
@@ -22,8 +24,8 @@ export class TestComponent implements OnInit {
   }
 
   fetchWorkOptions(): void {
-    this.workOptions$ = this.timesheetService.test().pipe(
-      map((data) => data ?? []) // Use nullish coalescing to ensure a default empty array
-    );
+    this.workOptions$ = this.timesheetService
+      .test()
+      .pipe(map((data) => data ?? []));
   }
 }
