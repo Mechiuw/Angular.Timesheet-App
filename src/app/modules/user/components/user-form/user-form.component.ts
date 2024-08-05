c8f9a7c3e952fa761f12659e08c563d8dea2ac1b
Muhammad Irfan Shiddiq <irfams99@gmail.com>
2024-08-05 19:04:12 +0700

    wip: :rocket: [FR1] add user form

--- src/app/modules/user/components/user-form/user-form.component.ts
+++ src/app/modules/user/components/user-form/user-form.component.ts
@@ -1,18 +1,20 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ButtonComponent } from "../../../../shared/components/button/button.component";
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AngularSvgIconModule } from 'angular-svg-icon';
-import { NgClass, NgIf } from '@angular/common';
-import { ToastModule } from 'primeng/toast';
-import { AuthService } from '../../../auth/services/auth.service';
 import { MessageService } from 'primeng/api';
 import { DropdownModule } from 'primeng/dropdown';
+import { ToastModule } from 'primeng/toast';
+import { ButtonComponent } from "../../../../shared/components/button/button.component";
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [FormsModule, DropdownModule, ReactiveFormsModule, RouterLink, AngularSvgIconModule, NgClass, NgIf, ButtonComponent, ToastModule],
+  imports: [FormsModule, FloatLabelModule, InputTextModule, DropdownModule, ReactiveFormsModule, RouterLink, AngularSvgIconModule, NgClass, NgIf, ButtonComponent, ToastModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.scss',
   providers: [MessageService]
@@ -28,7 +30,6 @@ export class UserFormComponent implements OnInit {
   constructor(
     private readonly formBuilder: FormBuilder, 
     private readonly router: Router,
-    private readonly authService: AuthService,
     private readonly messageService: MessageService
   ) {}
 
@@ -49,17 +50,13 @@ export class UserFormComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       name: ['', Validators.required],
       // role: ['', Validators.required],
-      selectedCity: new FormControl<{ name: string, code: string } | null>(null)
+      role: new FormControl<{ name: string, code: string } | null>(null)
 
     });
   }
 
   get f() {
     return this.form.controls;
-  }
-
-  togglePasswordTextType() {
-    this.passwordTextType = !this.passwordTextType;
   }
 
   onSubmit() {
@@ -71,20 +68,20 @@ export class UserFormComponent implements OnInit {
       return;
     }
 
-    this.authService.login({ email, password }).subscribe({
-      next: () => {
-        // console.log(this.authService.currentUser);
-        this.router.navigate(['/dashboard']);
-      },
-      error: (err) => {
-        // console.log(err.error.responseMessage);
-        this.messageService.add({
-          severity: 'warn',
-          summary: 'Warn',
-          detail: err.error.responseMessage,
-        })
-      },
-    });
+    // this.authService.login({ email, password }).subscribe({
+    //   next: () => {
+    //     // console.log(this.authService.currentUser);
+    //     this.router.navigate(['/dashboard']);
+    //   },
+    //   error: (err) => {
+    //     // console.log(err.error.responseMessage);
+    //     this.messageService.add({
+    //       severity: 'warn',
+    //       summary: 'Warn',
+    //       detail: err.error.responseMessage,
+    //     })
+    //   },
+    // });
 
     // this.authService.loginDummy().subscribe((token) => {
     //   console.log("SignIn.loginDummy : "+token);
