3986e2a4f8d2039f586c9c151d817557bb492195
Muhammad Irfan Shiddiq <irfams99@gmail.com>
2024-08-05 22:25:35 +0700

    wip: :rocket: [FR1] create user.service

--- src/app/modules/error/error.module.ts
+++ src/app/modules/error/error.module.ts
@@ -3,13 +3,15 @@ import { ErrorRoutingModule } from "./error-routing.module";
 import {
   provideHttpClient,
   withFetch,
+  withInterceptors,
   withInterceptorsFromDi,
 } from "@angular/common/http";
 import { AngularSvgIconModule } from "angular-svg-icon";
+import { requestInterceptor } from "../../core/interceptor/request.interceptor";
 
 @NgModule({
   declarations: [],
   imports: [ErrorRoutingModule, AngularSvgIconModule.forRoot()],
-  providers: [provideHttpClient(withFetch(), withInterceptorsFromDi())],
+  providers: [provideHttpClient(withFetch(),withInterceptors([requestInterceptor]))],
 })
 export class ErrorModule {}
