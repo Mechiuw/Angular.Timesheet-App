e2c2fb2610c0a4077b0302379c493d5dc46482ac
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-08-07 02:46:09 +0700

    [IMPROVE] relocated role dir

--- src/app/modules/role/component/dropdown/role.list.component.ts
+++ src/app/modules/role/component/dropdown/role.list.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { RoleService } from '../service/role.service';
-import { Role } from '../models/role.model';
+import { RoleService } from '../../service/role.service';
+import { Role } from '../../models/role.model';
 
 
 @Component({
