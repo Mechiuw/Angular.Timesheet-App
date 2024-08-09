8ecebf6e910f132b41015f9760d95b18eff42d8d
Fakhrul <barz.develop@gmail.com>
2024-08-09 19:50:09 +0700

    fix: :construction: date and time validator message

--- src/app/modules/timesheet/components/edit-form/edit.component.ts
+++ src/app/modules/timesheet/components/edit-form/edit.component.ts
@@ -175,7 +175,7 @@ export class EditComponent implements OnInit {
         new Date(`1970-01-01T${endTime}:00`) <
           new Date(`1970-01-01T${startTime}:00`)
       ) {
-        return { endTimeValidator: true };
+        return { checkTimeValidator: true };
       }
       return null;
     };
