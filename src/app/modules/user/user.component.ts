d342fb4bb9a33d7b47fbf05f2c6b23ca51744f3f
Muhammad Dzaki Era Davala <mdzakied123@gmail.com>
2024-08-12 05:18:33 +0700

    fix: :construction: [users module] (fix layout and service - with comment commit)

--- src/app/modules/user/user.component.ts
+++ src/app/modules/user/user.component.ts
@@ -1,23 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { UserFormComponent } from "./components/user-form/user-form.component";
-import { UserListComponent } from "./components/user-list/user-list.component";
-import { UserService } from './services/user.service';
+import { Component } from '@angular/core';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { TitleHeaderComponent } from '../../shared/components/title-header/title-header.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [UserFormComponent, UserListComponent],
+  imports: [UserFormComponent, UserListComponent, TitleHeaderComponent],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
-export class UserComponent implements OnInit {
-  constructor(
-    private readonly userService: UserService
-  ){
-
-  }
-  ngOnInit(): void {
-
-  }
-
+export class UserComponent {
+  // NFT Header
+  title: string = 'Master Data';
+  subtitle: string = 'Users';
 }
